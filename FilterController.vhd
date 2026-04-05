9282d1dc2787492c55438cf48ba3ba11d191440f
mccarthyannie <aelizabethm@mun.ca>
2026-04-05 17:16:03 -0230

    Replace RAM component with Goodbye World and clean up code

--- FilterController.vhd
+++ FilterController.vhd
@@ -63,15 +63,20 @@ signal current_state : state_type := IDLE;
 -- IMAGE FILTER
 -- VGA CONTROLLER
 
-component RAM is 
-	generic(ADDR_SPACE: INTEGER);
-	port ( CLK, we1, we2: in std_logic;
-          data_in1: in std_logic_vector(7 downto 0);
-			 data_in2: in std_logic_vector(7 downto 0);
-			 address1: in std_logic_vector((ADDR_SPACE-1) downto 0);
-          address2: in std_logic_vector((ADDR_SPACE-1) downto 0);
-          data_out1: out std_logic_vector(7 downto 0);
-          data_out2: out std_logic_vector(7 downto 0));
+component RAM_IP is
+	port
+	(
+		address_a		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		address_b		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		clock_a		: IN STD_LOGIC  := '1';
+		clock_b		: IN STD_LOGIC ;
+		data_a		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		data_b		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		wren_a		: IN STD_LOGIC  := '0';
+		wren_b		: IN STD_LOGIC  := '0';
+		q_a		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+		q_b		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+	);
 end component;
 	
 component FILTER_KERNALS is
@@ -113,61 +118,49 @@ end component;
 
 begin
 
-	ram_a : component RAM
-		generic map(ADDR_SPACE => 20)
-		port map(
-			CLK => CLOCK_50,
-			we1 => '0',
-			we2 => '0',
-			data_in1 => (others => '0'),
-			data_in2 => (others => '0'),
-			address1 => address1_a,
-			address2 => (others => '0'),
-			data_out1 => dataout1_a,
-			data_out2 => open);
-			
-	ram_b : component RAM
-		generic map(ADDR_SPACE => 20)
-		port map(
-			CLK => CLOCK_50,
-			we1 => we1_b,
-			we2 => '0',
-			data_in1 => datain1_b,
-			data_in2 => (others => '0'),
-			address1 => address1_b,
-			address2 => address2_b,
-			data_out1 => open,
-			data_out2 => dataout2_b);
-			
-	filter_kernal : component FILTER_KERNALS
+	ram_a : component RAM_IP
 		port map (
-			CLK => CLOCK_50,
-			en => kernal_en,
-			address => kernal_address,
-			data => kernal_data);
+			address_a => address1_a,
+			address_b => (others => '0'),
+			clock_a => VGA_CLK,
+			clock_b => CLOCK_50,
+			data_a => (others => '0'),
+			data_b => (others => '0'),
+			wren_a => '0',
+			wren_b => '0',
+			q_a => dataout1_a,
+			q_b => open
+		);
 			
-	img_filter : component ImageFilter
-    generic map(
-        IMG_HEIGHT    => 480,
-        IMG_WIDTH     => 640,
-        RAM_ADDR_SIZE => 20)
-    port map(
-        CLK        => CLOCK_50,
-        filter_en  => filter_en,
-        ram_in     => dataout1_a,
-        rom_in     => kernal_data,
-        filter_sel => FILTER_SEL(1 downto 0),
-        ram_addr   => filter_read_addr,
-        rom_addr   => kernal_address,
-        data_out   => datain1_b,
-        READY      => filter_ready);
+--	filter_kernal : component FILTER_KERNALS
+--		port map (
+--			CLK => CLOCK_50,
+--			en => kernal_en,
+--			address => kernal_address,
+--			data => kernal_data);
 			
+--	img_filter : component ImageFilter
+--    generic map(
+--        IMG_HEIGHT    => 480,
+--        IMG_WIDTH     => 640,
+--        RAM_ADDR_SIZE => 20)
+--    port map(
+--        CLK        => CLOCK_50,
+--        filter_en  => filter_en,
+--        ram_in     => dataout1_a,
+--        rom_in     => kernal_data,
+--        filter_sel => FILTER_SEL(1 downto 0),
+--        ram_addr   => filter_read_addr,
+--        rom_addr   => kernal_address,
+--        data_out   => datain1_b,
+--        READY      => filter_ready);
+--			
 	vga : component VGA
 		port map (
 			CLOCK_50 => CLOCK_50,
 			KEY => KEY,
-			pixel_data  => dataout2_b,
-			pixel_addr  => address2_a,
+			pixel_data  => dataout1_b,
+			pixel_addr  => vga_pixel_addr,
 			VGA_CLK => VGA_CLK,
 			VGA_HS => VGA_HS,
 			VGA_VS => VGA_VS,
@@ -177,50 +170,53 @@ begin
 			VGA_G => VGA_G,
 			VGA_B => VGA_B);
 			
+	 address1_a <= vga_pixel_addr(15 downto 0);
+			
     RESETb <= KEY(0);
     START <= KEY(1);
     FILTER_SEL <= SW(1 downto 0);
 	 kernal_en <= filter_en;
 	 
 	 process(CLOCK_50)
+	 begin
 		if CLOCK_50'event and CLOCK_50 = '1' then
 			if RESETb = '0' then
-				current_state = IDLE;
-				filter_en <= '0';
-				we1_b <= '0';
-			else:
-				case current_state is:
+				current_state <= IDLE;
+--				filter_en <= '0';
+--				we1_b <= '0';
+			else
+				case current_state is
 					when IDLE =>
-						filter_en <= '0';
-						we1_b <= '0';
+--						filter_en <= '0';
+--						we1_b <= '0';
 						
 						if START = '0' then
-							current_state <= FILTERING;
+							current_state <= DISPLAY;
 						end if;
 							
-					when FILTERING =>
-						filter_en <= '1';
-						we1_b <= '1';
-						if filter_ready = '1' then
-							current_state <= DISPLAY;
-							filter_en <= '0';
-							we1_b <= '0';
-						end if;
+--					when FILTERING =>
+--						filter_en <= '1';
+--						we1_b <= '1';
+--						if filter_ready = '1' then
+--							current_state <= DISPLAY;
+--							filter_en <= '0';
+--							we1_b <= '0';
+--						end if;
 							
 					when DISPLAY =>
 						if START = '1' then
-							buffer_select <= not buffer_select;
+--							buffer_select <= not buffer_select;
 							current_state <= IDLE;
 						end if;
 				end case;
 			end if;
 		end if;
 end process;
-	address1_a <= filter_read_addr  when buffer_select = '0' else filter_write_addr;
-	we1_a      <= '0'               when buffer_select = '0' else filter_we;
-
-	address1_b <= filter_write_addr when buffer_select = '0' else filter_read_addr;
-	we1_b      <= filter_we         when buffer_select = '0' else '0';
-	
-	vga_pixel_addr_to_ram <= address2_b when buffer_select = '0' else address2_a;
+--	address1_a <= filter_read_addr  when buffer_select = '0' else filter_write_addr;
+--	we1_a      <= '0'               when buffer_select = '0' else filter_we;
+--
+--	address1_b <= filter_write_addr when buffer_select = '0' else filter_read_addr;
+--	we1_b      <= filter_we         when buffer_select = '0' else '0';
+--	
+--	vga_pixel_addr_to_ram <= address2_b when buffer_select = '0' else address2_a;
 end arch;
